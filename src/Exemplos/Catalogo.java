333692db03b78862208d4edea83d411acb5c666a
26066539 <surama.queiroz90@gmail.com>
2019-02-21 20:02:34 -0300

    aula 21.02

---
+++ src/Exemplos/Catalogo.java
@@ -0,0 +1,35 @@
+package Exemplos;
+
+class Catalago {
+        public static void main(String[] args){
+            /*Estrutura de seleção*/
+            int a=10, b=90, c=-100;
+            if (a>0) {
+                a = c;
+            }
+            System.out.println(a);
+            if (c>a){
+                c=a;
+            }else {
+                c=b;
+            }
+            System.out.println(c);
+            /*switch*/
+            int op=3;
+            int v = 0;
+            switch (op) {
+                case 1:
+                    v = 1;
+                    break;
+                case 2:
+                    v = 2;
+                    break;
+                default:
+                    v = -1;
+            }
+            System.out.println(v);
+            }
+        }
+
+
+
