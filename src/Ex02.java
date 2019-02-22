d1ca93e228f986542301db738dbfb2fcbe430afb
26066539 <surama.queiroz90@gmail.com>
2019-02-21 21:05:38 -0300

    aula 21.02

---
+++ src/Ex02.java
@@ -0,0 +1,13 @@
+public class Ex02 {
+            public static void main (String[] args){
+    int totalFolha =100;
+    double resultado;
+
+    if (totalFolha <100) {
+        resultado = totalFolha * 0.25;
+    }else{
+        resultado = totalFolha*0.20;
+        System.out.println(totalFolha*resultado);
+    }
+}
+}
